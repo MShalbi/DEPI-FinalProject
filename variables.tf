50a8313d07d7c717bff0cfc58ab37d7f8e923a39
Mohamed Omda <iteng.kuc@gmail.com>
2024-10-20 17:47:59 +0300

    change OS image

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-0866a3c8686eaeeba"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
