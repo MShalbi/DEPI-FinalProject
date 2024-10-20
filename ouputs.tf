133d3fa47a1196afc4cabcf25114a8d162e9cc20
Mohamed Omda <iteng.kuc@gmail.com>
2024-10-20 19:38:21 +0300

    edit outputs.tf file

--- ouputs.tf
+++ ouputs.tf
@@ -1,6 +1,6 @@
-output "websiteendpoint" {
-    value = aws_s3_bucket.mybucket.website_endpoint
-}
+# output "websiteendpoint" {
+#     value = aws_s3_bucket.mybucket.website_endpoint
+# }
 
 output "public_ip" {
     value = aws_instance.Ajay.public_ip
